3de7520cdb6633ec092d042bdb0c9da256438180
beenpow <beenpow@gmail.com>
2025-05-16 14:39:22 +0900

    05/16 - 5th class - Adding main page to 'todo list'.

--- 20250512_Web_APP/src/routes/pages/Todos.tsx
+++ 20250512_Web_APP/src/routes/pages/Todos.tsx
@@ -3,9 +3,11 @@
 */
 import TodoList from '@/components/todos/TodoList'
 import TodoCreator from '@/components/todos/TodoCreator'
+import { Link } from 'react-router'
 
 export default function Todos() {
     return <>
+        <Link to="/">Main</Link>
         <TodoCreator />
         <TodoList />
     </>
